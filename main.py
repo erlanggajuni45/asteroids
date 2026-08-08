21b5177aa39dbfcabff1c6b0712f7830bc7a231f
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-08-08 13:19:55 +0700

    set FPS to 60

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame
 
 def main() -> None:
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -13,6 +15,7 @@ def main() -> None:
                 return
         screen.fill(color='black')
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
