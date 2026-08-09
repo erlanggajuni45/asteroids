f2d46e5e7f1eafee5cbf79f9de2124f0d3436a0c
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-08-09 13:24:54 +0700

    create shots group and set the shot class's container

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import pygame
 import sys
 
@@ -13,9 +14,12 @@ def main() -> None:
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
     Asteroid.containers = (asteroids, updatable, drawable)    
     Player.containers = (updatable, drawable)    
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
